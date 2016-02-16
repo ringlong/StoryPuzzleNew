ffd770143e4e61050678f98b2ce0e46bb8057389
任宏宇 <renhy@yiche.com>
2016-02-16 17:49:17 +0800

    add iAD

---
+++ Puzzle/Controllers/iAdViewController.m
@@ -0,0 +1,170 @@
+//
+//  iAdViewController.m
+//  Puzzle
+//
+//  Created by Andrea Barbon on 07/06/12.
+//  Copyright (c) 2012 Università degli studi di Padova. All rights reserved.
+//
+
+#import "iAdViewController.h"
+#import "PuzzleController.h"
+
+@interface iAdViewController ()
+
+@end
+
+@implementation iAdViewController
+
+@synthesize managedObjectContext, persistentStoreCoordinator, puzzle, adBannerView, prevOrientation, adPresent;
+
+
+#pragma mark -
+#pragma iAd
+
+- (void)viewDidLoad {
+    
+//    [self createAdBannerView];
+//    [self.view addSubview:adBannerView];
+    
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    
+    return YES;
+
+}
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+
+    adBannerView.hidden = YES;
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        
+        CGRect rect;
+        float screenWidth = [[UIScreen mainScreen] bounds].size.width;
+        float screenHeight = [[UIScreen mainScreen] bounds].size.height;
+        
+        
+        if (UIInterfaceOrientationIsPortrait(toInterfaceOrientation)) {
+            adBannerView.currentContentSizeIdentifier = ADBannerContentSizeIdentifierPortrait;
+            CGSize bannerSize = [ADBannerView sizeFromBannerContentSizeIdentifier:adBannerView.currentContentSizeIdentifier];
+            rect = CGRectMake(0, screenHeight-bannerSize.height*adBannerView.bannerLoaded, 0, 0);
+        } else {
+            adBannerView.currentContentSizeIdentifier = ADBannerContentSizeIdentifierLandscape;
+            CGSize bannerSize = [ADBannerView sizeFromBannerContentSizeIdentifier:adBannerView.currentContentSizeIdentifier];
+            rect = CGRectMake(0, screenWidth-bannerSize.height*adBannerView.bannerLoaded, 0, 0);
+        }
+        
+        adBannerView.frame = rect;
+        
+    }completion:^(BOOL finished) {
+        
+        adBannerView.hidden = NO;
+        
+    }];
+
+    [self.view bringSubviewToFront:self.adBannerView];
+    
+    
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
+   
+}
+
+
+- (void) createAdBannerView
+{
+    
+    CGRect rect;
+    float screenWidth = [[UIScreen mainScreen] bounds].size.width;
+    float screenHeight = [[UIScreen mainScreen] bounds].size.height;
+    
+    if (UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].statusBarOrientation)) {
+        rect = CGRectMake(0, screenHeight, 0, 0);
+    } else {
+        rect = CGRectMake(0, screenWidth, 0, 0);
+    }
+
+    adBannerView = [[ADBannerView alloc] initWithFrame:rect];
+    adBannerView.hidden = YES;
+    [self.view addSubview:adBannerView];
+    
+    adBannerView.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin |UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
+    
+    adBannerView.delegate = self;
+}
+/*
+#pragma mark - ADBannerViewDelegate
+
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner
+{
+    NSLog(@"------> AD");
+    [(PuzzleController*)self adjustForAd:!adPresent];
+    [self adjustBannerViewWithOrientation:[UIApplication sharedApplication].statusBarOrientation];
+    adPresent = YES;
+}
+
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
+    NSLog(@"AD error");
+    [(PuzzleController*)self adjustForAd:-1*adPresent];
+    [self adjustBannerViewWithOrientation:[UIApplication sharedApplication].statusBarOrientation];
+    adPresent = NO;
+}
+
+- (BOOL)bannerViewActionShouldBegin:(ADBannerView *)banner willLeaveApplication:(BOOL)willLeave
+{
+    IF_IPAD [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:YES];
+    prevOrientation = [UIApplication sharedApplication].statusBarOrientation;
+    return YES;
+}
+
+- (void)bannerViewActionDidFinish:(ADBannerView *)banner
+{
+    
+    if ([UIApplication sharedApplication].statusBarOrientation != prevOrientation) [(PuzzleController*)self fuckingRotateTo:[UIApplication sharedApplication].statusBarOrientation duration:0.5];
+    [self adjustBannerViewWithOrientation:[UIApplication sharedApplication].statusBarOrientation];
+    
+    IF_IPAD [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:YES];
+}
+
+
+- (void) adjustBannerViewWithOrientation:(UIInterfaceOrientation)orientation
+{
+    
+    CGRect rect;
+    float screenWidth = [[UIScreen mainScreen] bounds].size.width;
+    float screenHeight = [[UIScreen mainScreen] bounds].size.height;
+    
+    if (UIInterfaceOrientationIsPortrait(orientation)) {
+        adBannerView.currentContentSizeIdentifier = ADBannerContentSizeIdentifierPortrait;
+        CGSize bannerSize = [ADBannerView sizeFromBannerContentSizeIdentifier:adBannerView.currentContentSizeIdentifier];
+        rect = CGRectMake(0, screenHeight-bannerSize.height*adBannerView.bannerLoaded, 0, 0);
+    } else {
+        adBannerView.currentContentSizeIdentifier = ADBannerContentSizeIdentifierLandscape;
+        CGSize bannerSize = [ADBannerView sizeFromBannerContentSizeIdentifier:adBannerView.currentContentSizeIdentifier];
+        rect = CGRectMake(0, screenWidth-bannerSize.height*adBannerView.bannerLoaded, 0, 0);
+    }
+    
+    
+    [UIView animateWithDuration:0.5 animations:^{        
+        
+        adBannerView.frame = rect;
+        
+    }completion:^(BOOL finished) {
+        
+        adBannerView.hidden = NO;
+        
+    }];
+    
+
+    [self.view bringSubviewToFront:self.adBannerView];
+    
+    return;
+        
+    
+}
+ */
+
+@end
